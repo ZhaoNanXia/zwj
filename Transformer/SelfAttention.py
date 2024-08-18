e1f44ec917581de90b3150c900d6a1487b23a15b
wanjie <zhaowanjie_123@163.com>
2024-08-18 17:19:46 +0800

    add self_Attention

--- Transformer/SelfAttention.py
+++ Transformer/SelfAttention.py
@@ -3,17 +3,42 @@ import torch.nn as nn
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, d_model, dropout=0.0):
-        super(SelfAttention).__init__()
+    def __init__(self, dim, dk, dv, dropout=0.0):
+        super().__init__()
 
-        self.d_model = d_model
+        self.scale = dk ** -0.5
+        self.q = nn.Linear(dim, dk)
+        self.k = nn.Linear(dim, dk)
+        self.v = nn.Linear(dim, dv)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, q, k, v, mask=None):
-        attn = torch.matmul(q / self.d_model, k.transpose(2, 3))
-        if mask is not None:
-            attn = attn.masked_fill(mask == 0, -1e9)
-        attn_softmax = nn.Softmax()
-        attn = self.dropout()
-        output = torch.matmul(attn_softmax, v)
-        return output, attn
+    def forward(self, x):
+        # x: [1, 4, 2]——>[batch_size, num_token, seq_length]
+        q = self.q(x)  # q:[1, 4, 3]——>[batch_size, num_token, dk]
+        print('q:', q)
+        k = self.k(x)  # k:[1, 4, 3]——>[batch_size, num_token, dk]
+        print('k:', k)
+        v = self.v(x)  # k:[1, 4, 4]——>[batch_size, num_token, dv]
+        print('v:', v)
+
+        # attn_score = softmax((q*k^T)*(dk^-0.5))*v
+        attn = torch.matmul(q * self.scale, k.transpose(-2, -1))
+        print('attn:', attn)
+        # attn是一个torch.Tensor对象，所以可以直接调用softmax
+        attn = attn.softmax(dim=-1)
+        print('attn_score:', attn)
+        attn = self.dropout(attn)
+        print('attn_dropout:', attn)
+        output = torch.matmul(attn, v)
+
+        return output
+
+
+# 模型输入参数包括：输入数据的维度, 映射之后的dk,dv维度大小
+model = SelfAttention(dim=2, dk=3, dv=4)
+# 随机生成一个输入样本，1是batch_size, 4是指4个token，2是每个token的长度
+input_data = torch.randn(1, 4, 2)
+print('input_data:', input_data)
+final_output = model(input_data)
+print('final_output:', final_output)
+
